0649e6d42eb3b42c17d31286c714521ac0170391
EmilyOwens <emily.f.owens@gmail.com>
2014-06-11 11:40:24 -0600

    Parser now searches for both 'type' and 'subtype'

--- TextParser/textParser.cs
+++ TextParser/textParser.cs
@@ -1,54 +1,45 @@
 // Namespace Declaration
 using System;
 using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
 
 
 
 class TextParser
 {
-	// Reads in a string (a timestep) with format [(type, position, length), (type, position, length)...].
-	// It takes each component (type, position, length) and separates out the components into a list of strings called 'ar'.
-	public static void read_time_step(string s)
+	// Reads in a string (a timestep) with format [(type, subtype, position, length), (type, subtype, position, length)...].
+	// It takes each component (type, subtype, position, length) and separates out the components into a list of strings called 'ObjectArray'.
+	public static void read_time_step(string input)
 	{
 		string pattern = @"\(((.*?))\)";
-		string[] ar;
+		string[] ObjectArray;
 		
-		foreach(Match match in Regex.Matches(s, pattern, RegexOptions.IgnoreCase))
+		foreach(Match match in Regex.Matches(input, pattern, RegexOptions.IgnoreCase))
 		{
-			ar = (match.Value).Split (new Char[] {' '});
-			Console.WriteLine("Found type: '{0}', Found position: '{1}', Found length '{2}'", ar[0], ar[1], ar[2]);
+			ObjectArray = (match.Value).Split (new Char[] {' '});
+			Console.WriteLine("Found type: '{0}', Found subtype: '{1}', Found position: '{2}', Found length '{3}'", ObjectArray[0], ObjectArray[1], ObjectArray[2], ObjectArray[3]);
 			Console.WriteLine();
 		}	
 	}
 
 	static void Main()
 	{
-		Console.WriteLine("Reading the contents from the test.txt file\n");
+		Console.WriteLine("Reading the contents from the test file\n");
 
 		// Use stream object to open and read file
-		StreamReader s = File.OpenText ("test2.txt");
+		StreamReader s = File.OpenText ("test3.txt");
 
 		//string 'buffer' used to hold streamed 
 		string read = null;
-
-		// Search Target = String we want to locate
-		string searchTarget = "hello";
-
 
 		//*************PARSING LOGIC************//
 		
 		
 		while((read = s.ReadLine()) != null)		//Reads the whole line
 		{
-			
-			if (read.Contains (searchTarget)) 
-			{
-				// Should find EACH instance of searchTarget
-				
-				Console.WriteLine (searchTarget);	// Currently writes first instance of searchTarget
-				
-				Console.WriteLine(read.IndexOf(searchTarget));	// Currently writes position in string of first instance of searchTarget
-			}
+
+			read_time_step (read);	
 			
 		}
 				
