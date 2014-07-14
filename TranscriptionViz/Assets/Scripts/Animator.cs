03cee3e88d4ead1313920adf5b39f8cebb33220c
chadbryant7 <chadbryant7@gmail.com>
2014-07-14 12:20:55 -0600

    Changed animation class name

---
+++ TranscriptionViz/Assets/Scripts/Animator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animator : MonoBehaviour
+{
+
+
+
+		// Use this for initialization
+		void Start ()
+		{
+			GameObject nucleosome  = new GameObject();
+			string move = "(1,2,3)";
+			string paren = "(";
+			InstructionObject IO = new InstructionObject(nucleosome, move);
+			Debug.Log(nucleosome.transform.position);
+ 
+			if(IO.instruction.StartsWith(paren))
+			{
+				Debug.Log ("has paren");
+				IO.TranscriptionSimObject.transform.position = 0;
+			}
+			else
+			{
+				Debug.Log("no create");
+			}
+				
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
