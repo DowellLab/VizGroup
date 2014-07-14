97413cf99cbc18351906b90df15ec51e6812f39b
chadbryant7 <chadbryant7@gmail.com>
2014-07-14 11:53:56 -0600

    Merge

---
+++ TranscriptionViz/Assets/Scripts/Animation.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animation : MonoBehaviour
+{
+
+
+
+		// Use this for initialization
+		void Start ()
+		{
+				string chad = "create 4";
+				string create = "create"; 
+				if(chad.StartsWith(create))
+				{
+					Debug.Log (chad);
+				}
+				else
+				{
+					Debug.Log("no create");
+				}
+				
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
