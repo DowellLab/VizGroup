7aec356e3e987b575ae4a62e78ce4e1f54853254
Malcolm Duren <madu7833@colorado.edu>
2014-07-14 12:33:12 -0600

    Initial commit of params data structure -Also some reorganizing of interface files

---
+++ TranscriptionViz/Assets/Scripts/User Interface/simParams.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+class simParams
+{
+	public class clssimParams
+	{
+		//Dictionary of data
+		public Dictionary<string, Dictionary<string, int>> dict;
+
+
+		public clssimParams()
+		{
+			dict = new Dictionary<string, Dictionary<string, int>> ();
+
+			dict.Add ("DNA", new Dictionary<string, int> ());
+
+			dict ["DNA"].Add ("count", 3);
+			dict ["DNA"].Add ("color", 4);
+			dict ["DNA"].Add ("size", 5);
+		}
+
+		public void test()
+		{
+			int n = dict["DNA"]["count"];
+
+			Console.Write (n);
+
+		}
+	};
+
+	static void main()
+	{
+		clssimParams c = new clssimParams ();
+		c.test ();
+
+	}
+
+}
