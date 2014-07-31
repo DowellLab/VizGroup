ef054769760de4b0df0f3dcb3f9e2abcf0f4cfe1
EmilyOwens <emily.f.owens@gmail.com>
2014-07-31 13:24:38 -0600

    Adds Camera Zoom

---
+++ TranscriptionViz/Assets/Scripts/cameraZoom.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraZoom : MonoBehaviour {
+
+	public float zoomSpeed = 20;
+
+	public float minZoom = 30;
+	public float maxZoom = 150;
+
+	void Update ()
+	{
+
+		float scroll = Input.GetAxis("Mouse ScrollWheel");
+		if (scroll != 0.0f)
+		{
+
+				camera.fieldOfView -= scroll*zoomSpeed;
+
+				camera.fieldOfView = Mathf.Clamp(camera.fieldOfView, minZoom, maxZoom);
+
+				Debug.Log (camera.fieldOfView);
+
+		}
+	}
+
+}
