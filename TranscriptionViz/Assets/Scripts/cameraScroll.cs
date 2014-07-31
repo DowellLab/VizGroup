0ad474bfd486d75c25154a6d7c2826939098cbe9
MalcolmD <malcolm.duren@colorado.edu>
2014-07-31 13:28:22 -0600

    Scrolling not affected by time scale

--- TranscriptionViz/Assets/Scripts/cameraScroll.cs
+++ TranscriptionViz/Assets/Scripts/cameraScroll.cs
@@ -6,13 +6,15 @@ public class cameraScroll : MonoBehaviour {
 	public int scrollArea;
 	private float scrollSpeed;
 	public float scrollForce;
+	private float dTime;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		scrollArea = 50;
-		scrollSpeed = 1;
+		scrollArea = 30;
+		scrollSpeed = 5;
 		scrollForce = 10;
+		dTime = 0.0035f;
 	}
 
 
@@ -20,7 +22,7 @@ public class cameraScroll : MonoBehaviour {
 	void Update () 
 	{
 		//Scroll  push, means it screen movement will accelerate
-		scrollSpeed += (scrollForce * Time.deltaTime);
+		scrollSpeed += (scrollForce * dTime);
 
 
 
@@ -29,8 +31,8 @@ public class cameraScroll : MonoBehaviour {
 
 
 		// Do camera movement by mouse position
-		if (mPosX < scrollArea) {transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);}
-		if (mPosX >= Screen.width-scrollArea) {transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);}
-		if (mPosX > scrollArea & mPosX < Screen.width - scrollArea) { scrollSpeed = 0; }
+		if (mPosX < scrollArea) {transform.Translate(Vector3.right * -scrollSpeed * dTime);}
+		if (mPosX >= Screen.width-scrollArea) {transform.Translate(Vector3.right * scrollSpeed * dTime);}
+		if (mPosX > scrollArea & mPosX < Screen.width - scrollArea) { scrollSpeed = 5; }
 	}
 }
